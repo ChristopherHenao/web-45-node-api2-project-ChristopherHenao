ac5bba83f303d9b69be5244c078ae29f5c222ddb
ChristoherHenao <cq.henao@gmail.com>
2021-08-31 15:18:00 -0400

    basic endpoint structures built

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,7 +4,34 @@ const Posts = require('./posts-model')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.json('get is working')
+    res.json('the get posts is working')
 })
+
+
+router.get('/:id', (req, res) => {
+    res.json('the get by ID is working')
+})
+
+
+router.post('/', (req, res) => {
+    res.json('the post is working')
+})
+
+
+router.put('/:id', (req, res) => {
+    res.json('the put is working')
+})
+
+
+router.delete('/:id', (req, res) => {
+    res.json('the delete is working')
+})
+
+
+router.get('/:id/comments', (req, res) => {
+    res.json('the get comments is working')
+})
+
+
 
 module.exports = router
