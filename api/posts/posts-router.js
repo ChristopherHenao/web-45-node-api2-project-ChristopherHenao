77871f88b4977e08efdc31caf129242acf24f2e1
ChristoherHenao <cq.henao@gmail.com>
2021-08-31 15:03:05 -0400

    server built, connected and launched

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,10 @@
 // implement your posts router here
+const express = require('express')
+const Posts = require('./posts-model')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.json('get is working')
+})
+
+module.exports = router
